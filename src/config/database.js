3f6e0b6e8459e1b11049d4b6ee57f0dcf39f57d7
liviaher <liviaher.oliveira@gmail.com>
2023-05-31 02:38:04 -0300

    fix build command

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
     dialect : 'mysql',
-    host: process.env.MYSQLHOST,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-    username: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
+    host: "containers-us-west-131.railway.app",
+    username:'root',
+    password:"vbFSVeoEpodlerrh7Lei",
+    database: "railway",
+    port: "8063",
     define: {
         timestamps: true,
         underscored: true,
