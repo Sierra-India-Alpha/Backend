f3562ece3834676647d53773ed988f3fb45ce833
liviaher <liviaher.oliveira@gmail.com>
2023-05-31 03:00:00 -0300

    use enviment variable port

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 require('./database');
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen('3000', () => {
+app.listen(port, "0.0.0.0", () => {
     console.log('Server is running ...');
   });
 
